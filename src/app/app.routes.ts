08b631758a28c25262ec1bad0a3428f6238aebbb
JoaoGH <gabrielhartmannqwert@gmail.com>
2024-02-04 11:14:41 -0300

    feat: criar component acoes data
    esse componente conterá a listagem de todas as ações por determinada data.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { AcaoDiaComponent } from "./components/acao-dia/acao-dia.component";
+import { AcoesDataComponent } from "./components/acoes-data/acoes-data.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'acao-dia', component: AcaoDiaComponent }
+  { path: 'acao-dia', component: AcaoDiaComponent },
+  { path: 'acoes-data', component: AcoesDataComponent },
 ];
---
+++ src/app/components/acoes-data/acoes-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acoes-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './acoes-data.component.html',
+  styleUrl: './acoes-data.component.scss'
+})
+export class AcoesDataComponent {
+
+}
