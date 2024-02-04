4e4b32d4064306ae720faad82e82110cfed62d32
JoaoGH <gabrielhartmannqwert@gmail.com>
2024-02-04 11:56:53 -0300

    feat: adicionar pagina 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { AcaoDiaComponent } from "./components/acao-dia/acao-dia.component";
 import { AcoesDataComponent } from "./components/acoes-data/acoes-data.component";
+import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'acao-dia', component: AcaoDiaComponent },
   { path: 'acoes-data', component: AcoesDataComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
