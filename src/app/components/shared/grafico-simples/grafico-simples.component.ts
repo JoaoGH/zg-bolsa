a6fcc19b7fa95303d0bc84c02db84ecb613aaba6
JoaoGH <gabrielhartmannqwert@gmail.com>
2024-02-05 12:15:36 -0300

    feat: alimentar grafico

--- src/app/components/shared/grafico-simples/grafico-simples.component.ts
+++ src/app/components/shared/grafico-simples/grafico-simples.component.ts
@@ -7,6 +7,15 @@ import {
   MatDialogTitle
 } from "@angular/material/dialog";
 import {MatButtonModule} from "@angular/material/button";
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import {AcaoService} from "../../../services/acao.service";
+
+export interface Dados {
+  acao: string,
+  data: string
+}
 
 @Component({
   selector: 'app-grafico-simples',
@@ -17,6 +26,9 @@ import {MatButtonModule} from "@angular/material/button";
     MatDialogContent,
     MatDialogActions,
     MatDialogClose,
+    CommonModule,
+    RouterOutlet,
+    CanvasJSAngularChartsModule
   ],
   templateUrl: './grafico-simples.component.html',
   styleUrl: './grafico-simples.component.scss'
@@ -24,12 +36,38 @@ import {MatButtonModule} from "@angular/material/button";
 export class GraficoSimplesComponent implements OnInit {
   constructor(
     @Inject(MAT_DIALOG_DATA)
-    public data: {},
+    public data: Dados,
+    private acaoService: AcaoService
   ) {}
 
+  chartOptions: object = {}
+  dataPoints: object[] = []
+  notFound: string = ''
+
   ngOnInit(): void {
-    console.log('wjbef');
-    console.log(this.data);
+
+    this.acaoService.obterAcaoByDia(this.data.acao, this.data.data).then((value: any) => {
+      if (!value.success) {
+        this.notFound = `Não foi encontrado nenhum registro para ${this.data.acao} na data de ${this.data.data}.`
+        return
+      } else {
+        this.dataPoints = [
+          {label: "Compra", y: 10},
+          {label: "Venda", y: 15}
+        ]
+      }
+
+      this.chartOptions = {
+        title: {
+          text: `${this.data.acao} em ${this.data.data}`
+        },
+        data: [{
+          type: "column",
+          dataPoints: this.dataPoints
+        }]
+      };
+    });
+
   }
 
 }
