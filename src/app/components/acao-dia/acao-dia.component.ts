48ed741d199a3f24813acb1e2fba4161f21780ad
JoaoGH <gabrielhartmannqwert@gmail.com>
2024-02-05 10:26:33 -0300

    feat: abrir modal passando dados de tela.

--- src/app/components/acao-dia/acao-dia.component.ts
+++ src/app/components/acao-dia/acao-dia.component.ts
@@ -5,17 +5,17 @@ import {MatIcon} from "@angular/material/icon";
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
+//
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {Acao} from "../../models/Acao";
 import {AcaoService} from "../../services/acao.service";
 import {MatDatepickerInputEvent, MatDatepickerModule} from '@angular/material/datepicker';
-import {provideNativeDateAdapter} from '@angular/material/core';
-//
-
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
 import {provideMomentDateAdapter} from '@angular/material-moment-adapter';
 import * as _moment from 'moment';
 import {default as _rollupMoment} from 'moment';
+import {MatDialog} from "@angular/material/dialog";
+import {GraficoSimplesComponent} from "../shared/grafico-simples/grafico-simples.component";
+
 const moment = _rollupMoment || _moment;
 
 // See the Moment.js docs for the meaning of these formats:
@@ -55,10 +55,13 @@ export const MY_FORMATS = {
 export class AcaoDiaComponent implements OnInit {
   acoes: Acao[] = [];
   selectedAcao: string = '';
-  events: string[] = [];
+  selectedData: string = '';
   date = new FormControl(moment());
 
-  constructor(private acaoService: AcaoService) { }
+  constructor(
+    private acaoService: AcaoService,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
     this.acaoService.obterAcoes().then((data: Acao[]) => {
@@ -69,10 +72,11 @@ export class AcaoDiaComponent implements OnInit {
     });
   }
 
-  addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
-    const dataSelecionada = moment(event.value).format('DD/MM/yyyy')
-    console.log(dataSelecionada);
-    this.events.push(`${type}: ${event.value}`);
+  openDialog(): void {
+    this.selectedData = moment(this.date.value).format('DD/MM/yyyy');
+    this.dialog.open(GraficoSimplesComponent, {
+      data: { acao: this.selectedAcao, data: this.selectedData },
+    });
   }
 
 }
