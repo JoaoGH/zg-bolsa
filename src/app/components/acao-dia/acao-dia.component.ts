a593b676ab852b2e361157908597f13188ed4d09
JoaoGH <gabrielhartmannqwert@gmail.com>
2024-02-05 09:48:44 -0300

    feat: adicionar selecao de data formatada no padrao br

--- src/app/components/acao-dia/acao-dia.component.ts
+++ src/app/components/acao-dia/acao-dia.component.ts
@@ -8,6 +8,29 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {Acao} from "../../models/Acao";
 import {AcaoService} from "../../services/acao.service";
+import {MatDatepickerInputEvent, MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+//
+
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {provideMomentDateAdapter} from '@angular/material-moment-adapter';
+import * as _moment from 'moment';
+import {default as _rollupMoment} from 'moment';
+const moment = _rollupMoment || _moment;
+
+// See the Moment.js docs for the meaning of these formats:
+// https://momentjs.com/docs/#/displaying/format/
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-acao-dia',
@@ -20,6 +43,11 @@ import {AcaoService} from "../../services/acao.service";
     MatInputModule,
     MatSelectModule,
     FormsModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    provideMomentDateAdapter(MY_FORMATS),
   ],
   templateUrl: './acao-dia.component.html',
   styleUrl: './acao-dia.component.scss'
@@ -27,8 +55,16 @@ import {AcaoService} from "../../services/acao.service";
 export class AcaoDiaComponent implements OnInit {
   acoes: Acao[] = [];
   selectedAcao: string = '';
+  events: string[] = [];
+  date = new FormControl(moment());
 
   constructor(private acaoService: AcaoService) { }
+
+  addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
+    const dataSelecionada = moment(event.value).format('DD/MM/yyyy')
+    console.log(dataSelecionada);
+    this.events.push(`${type}: ${event.value}`);
+  }
 
   ngOnInit(): void {
     this.acaoService.obterAcoes().then((data: Acao[]) => {
